3ee33ca58e5515bba53253468933c9a3f2126efb
Karan <57486333+enso360@users.noreply.github.com>
2025-06-15 22:41:41 +0530

    simple tcl script

---
+++ partial_product_adder/Vivado_projects/thermometer_sequence_ppa/mycustom_simulation_run.tcl
@@ -0,0 +1,16 @@
+
+# Launch simulation
+launch_simulation
+
+# Restart simulation cleanly
+# restart
+
+close_wave_config
+
+# Load waveform configuration (adjust path as needed)
+open_wave_config "E:/Karan/Github_repos/mtp_files/partial_product_adder/Vivado_projects/thermometer_sequence_ppa/tb_column_adder_array_behav.wcfg"
+
+# Run simulation for a fixed time
+run 3000 ns
+
+
